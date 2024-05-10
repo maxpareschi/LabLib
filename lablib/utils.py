91fd05b27be59e9dd5bd4154bd8420978b5ca258
doerp <me@doerp.cool>
2024-05-10 21:00:17 +0200

    ruff formatting

--- lablib/utils.py
+++ lablib/utils.py
@@ -14,7 +14,8 @@ from .operators import ImageInfo
 
 class format_dict(dict):
     _placeholder = "**MISSING**"
-    def __missing__(self, key) -> str: 
+
+    def __missing__(self, key) -> str:
         return self._placeholder
 
 
@@ -23,7 +24,7 @@ def read_image_info(
     default_timecode: str = None,
     default_fps: float = None,
     default_par: float = None,
-    default_channels: int = None
+    default_channels: int = None,
 ) -> ImageInfo:
     if default_timecode is None:
         default_timecode = "01:00:00:01"
@@ -47,72 +48,78 @@ def read_image_info(
         "channels": None,
         "fps": None,
         "par": None,
-        "timecode": None
+        "timecode": None,
     }
 
     iinfo_res = deepcopy(result)
     ffprobe_res = deepcopy(result)
 
-    iinfo_cmd = [
-        "iinfo",
-        "-v",
-        abspath
-    ]
+    iinfo_cmd = ["iinfo", "-v", abspath]
 
     ffprobe_cmd = [
         "ffprobe",
-        "-v", "error",
-        "-select_streams", "v:0",
-        "-show_entries", "format_tags=timecode:stream_tags=timecode:stream=width,height,r_frame_rate,sample_aspect_ratio",
-        "-of", "default=noprint_wrappers=1",
-        abspath
+        "-v",
+        "error",
+        "-select_streams",
+        "v:0",
+        "-show_entries",
+        "format_tags=timecode:stream_tags=timecode:stream=width,height,r_frame_rate,sample_aspect_ratio",
+        "-of",
+        "default=noprint_wrappers=1",
+        abspath,
     ]
 
-    iinfo_out = subprocess.run(
-        iinfo_cmd,
-        capture_output=True,
-        text=True
-    ).stdout.strip().splitlines()
-    
-    ffprobe_out = subprocess.run(
-        ffprobe_cmd,
-        capture_output=True,
-        text=True
-    ).stdout.strip().splitlines()
-    
+    iinfo_out = (
+        subprocess.run(iinfo_cmd, capture_output=True, text=True)
+        .stdout.strip()
+        .splitlines()
+    )
+
+    ffprobe_out = (
+        subprocess.run(ffprobe_cmd, capture_output=True, text=True)
+        .stdout.strip()
+        .splitlines()
+    )
+
     for l in iinfo_out:
         if abspath in l and l.find(abspath) < 2:
             vars = l.split(": ")[1].split(",")
             size = vars[0].strip().split("x")
             channels = vars[1].strip().split(" ")
-            iinfo_res.update({
-                "width": int(size[0].strip()),
-                "height": int(size[1].strip()),
-                "display_width": int(size[0].strip()),
-                "display_height": int(size[1].strip()),
-                "channels": int(channels[0].strip())
-            })
+            iinfo_res.update(
+                {
+                    "width": int(size[0].strip()),
+                    "height": int(size[1].strip()),
+                    "display_width": int(size[0].strip()),
+                    "display_height": int(size[1].strip()),
+                    "channels": int(channels[0].strip()),
+                }
+            )
         if "FramesPerSecond" in l or "framesPerSecond" in l:
             vars = l.split(": ")[1].strip().split(" ")[0].split("/")
-            iinfo_res.update({
-                "fps": float(round(float(int(vars[0])/int(vars[1])), 3))
-            })
+            iinfo_res.update(
+                {"fps": float(round(float(int(vars[0]) / int(vars[1])), 3))}
+            )
         if "full/display size" in l:
             size = l.split(": ")[1].split("x")
-            iinfo_res.update({
-                "display_width": int(size[0].strip()),
-                "display_height": int(size[1].strip())
-            })
+            iinfo_res.update(
+                {
+                    "display_width": int(size[0].strip()),
+                    "display_height": int(size[1].strip()),
+                }
+            )
         if "pixel data origin" in l:
             origin = l.split(": ")[1].strip().split(",")
-            iinfo_res.update({
-                "origin_x": int(origin[0].replace("x=", "").strip()),
-                "origin_y": int(origin[1].replace("y=", "").strip())
-            })
+            iinfo_res.update(
+                {
+                    "origin_x": int(origin[0].replace("x=", "").strip()),
+                    "origin_y": int(origin[1].replace("y=", "").strip()),
+                }
+            )
         if "smpte:TimeCode" in l:
             iinfo_res["timecode"] = l.split(": ")[1].strip()
         if "PixelAspectRatio" in l:
-            iinfo_res["par"] =  float(l.split(": ")[1].strip())
+            iinfo_res["par"] = float(l.split(": ")[1].strip())
 
     for l in ffprobe_out:
         vars = l.split("=")
@@ -122,13 +129,15 @@ def read_image_info(
             ffprobe_res["display_height"] = int(vars[1].strip())
         if "r_frame_rate" in vars[0]:
             rate = vars[1].split("/")
-            ffprobe_res["fps"] = float(round(float(int(rate[0].strip())/int(rate[1].strip())), 3))
+            ffprobe_res["fps"] = float(
+                round(float(int(rate[0].strip()) / int(rate[1].strip())), 3)
+            )
         if "timecode" in l:
             ffprobe_res["timecode"] = vars[1]
         if "sample_aspect_ratio" in l:
             par = vars[1].split(":")
             if vars[1] != "N/A":
-                ffprobe_res["par"] = float(int(par[0].strip())/int(par[1].strip()))
+                ffprobe_res["par"] = float(int(par[0].strip()) / int(par[1].strip()))
             else:
                 ffprobe_res["par"] = 1
 
@@ -161,9 +170,7 @@ def read_image_info(
     return image_info
 
 
-def offset_timecode(
-    tc: str, frame_offset: int = None, fps: float = None
-) -> str:
+def offset_timecode(tc: str, frame_offset: int = None, fps: float = None) -> str:
     if not frame_offset:
         frame_offset = -1
     if not fps:
@@ -172,26 +179,19 @@ def offset_timecode(
     rationaltime = otio.opentime.from_timecode(tc, fps)
     frames = rationaltime.to_frames(fps) + frame_offset
     computed_rationaltime = otio.opentime.from_frames(frames, fps)
-    return otio.opentime.to_timecode(
-        computed_rationaltime,
-        fps,
-        is_drop
-    )
+    return otio.opentime.to_timecode(computed_rationaltime, fps, is_drop)
 
 
 def get_staging_dir() -> str:
-    return Path(
-        os.environ.get("TEMP", os.environ["TMP"]),
-        "lablib",
-        str(uuid.uuid4())
-    ).resolve().as_posix()
+    return (
+        Path(os.environ.get("TEMP", os.environ["TMP"]), "lablib", str(uuid.uuid4()))
+        .resolve()
+        .as_posix()
+    )
 
 
 def zero_matrix() -> List[List[float]]:
-    return [
-        [0.0] * 3
-        for _ in range(3)
-    ]
+    return [[0.0] * 3 for _ in range(3)]
 
 
 def identity_matrix() -> List[List[float]]:
@@ -199,30 +199,18 @@ def identity_matrix() -> List[List[float]]:
 
 
 def translate_matrix(t: List[float]) -> List[List[float]]:
-    return [
-        [1.0, 0.0, t[0]],
-        [0.0, 1.0, t[1]],
-        [0.0, 0.0, 1.0]
-    ]
+    return [[1.0, 0.0, t[0]], [0.0, 1.0, t[1]], [0.0, 0.0, 1.0]]
 
 
 def rotate_matrix(r: float) -> List[List[float]]:
     rad = math.radians(r)
     cos = math.cos(rad)
     sin = math.sin(rad)
-    return [
-        [cos, -sin, 0.0],
-        [sin, cos, 0.0],
-        [0.0, 0.0, 1.0]
-    ]
+    return [[cos, -sin, 0.0], [sin, cos, 0.0], [0.0, 0.0, 1.0]]
 
 
 def scale_matrix(s: List[float]) -> List[List[float]]:
-    return [
-        [s[0], 0.0, 0.0],
-        [0.0, s[1], 0.0],
-        [0.0, 0.0, 1.0]
-    ]
+    return [[s[0], 0.0, 0.0], [0.0, s[1], 0.0], [0.0, 0.0, 1.0]]
 
 
 def mirror_matrix(x: bool = False) -> List[List[float]]:
@@ -230,23 +218,14 @@ def mirror_matrix(x: bool = False) -> List[List[float]]:
     return scale_matrix(direction)
 
 
-def mult_matrix(
-    m1: List[List[float]],
-    m2: List[List[float]]
-) -> List[List[float]]:
+def mult_matrix(m1: List[List[float]], m2: List[List[float]]) -> List[List[float]]:
     return [
-        [
-            sum(a * b for a, b in zip(m1_row, m2_col))
-            for m2_col in zip(*m2)
-        ]
+        [sum(a * b for a, b in zip(m1_row, m2_col)) for m2_col in zip(*m2)]
         for m1_row in m1
     ]
 
 
-def mult_matrix_vector(
-    m: List[List[float]],
-    v: List[float]
-) -> List[float]:
+def mult_matrix_vector(m: List[List[float]], v: List[float]) -> List[float]:
     result = [0.0, 0.0, 0.0]
     for i in range(len(m)):
         for j in range(len(v)):
@@ -256,10 +235,7 @@ def mult_matrix_vector(
 
 def flip_matrix(w: float) -> List[List[float]]:
     result = identity_matrix()
-    chain = [
-        translate_matrix([w, 0.0]),
-        mirror_matrix(x=True)
-    ]
+    chain = [translate_matrix([w, 0.0]), mirror_matrix(x=True)]
     for m in chain:
         result = mult_matrix(result, m)
     return result
@@ -267,10 +243,7 @@ def flip_matrix(w: float) -> List[List[float]]:
 
 def flop_matrix(h: float) -> List[List[float]]:
     result = identity_matrix()
-    chain = [
-        translate_matrix([0.0, h]),
-        mirror_matrix()
-    ]
+    chain = [translate_matrix([0.0, h]), mirror_matrix()]
     for m in chain:
         result = mult_matrix(result, m)
     return result
@@ -310,33 +283,16 @@ def matrix_to_csv(m: List[List[float]]) -> str:
 
 
 def matrix_to_cornerpin(
-    m: List[List[float]],
-    w: int,
-    h: int,
-    origin_upperleft: bool = True
+    m: List[List[float]], w: int, h: int, origin_upperleft: bool = True
 ) -> List:
     cornerpin = []
     if origin_upperleft:
-        corners = [
-            [0, h, 1],
-            [w, h, 1],
-            [0, 0, 1],
-            [w, 0, 1]
-        ]
+        corners = [[0, h, 1], [w, h, 1], [0, 0, 1], [w, 0, 1]]
     else:
-        corners = [
-            [0, 0, 1],
-            [w, 0, 1],
-            [0, h, 1],
-            [w, h, 1]
-        ]
+        corners = [[0, 0, 1], [w, 0, 1], [0, h, 1], [w, h, 1]]
+    transformed_corners = [mult_matrix_vector(m, corner) for corner in corners]
     transformed_corners = [
-        mult_matrix_vector(m, corner)
-        for corner in corners
-    ]
-    transformed_corners = [
-        [corner[0] / corner[2], corner[1] / corner[2]]
-        for corner in transformed_corners
+        [corner[0] / corner[2], corner[1] / corner[2]] for corner in transformed_corners
     ]
     for corner in transformed_corners:
         cornerpin.extend(corner)
@@ -344,10 +300,7 @@ def matrix_to_cornerpin(
 
 
 def calculate_matrix(
-    t: List[float],
-    r: float,
-    s: List[float],
-    c: List[float]
+    t: List[float], r: float, s: List[float], c: List[float]
 ) -> List[List[float]]:
     c_inv = [-c[0], -c[1]]
     center = translate_matrix(c)
